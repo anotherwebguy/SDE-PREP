872d5c66f83219c2e04cdddbcbe5b746df78b824
Mohit singh <mohitrajputms123@gmail.com>
2022-06-02 09:40:09 +0530

    Create Rotate Matrix.cpp

---
+++ Day 2: Arrays Part II/Rotate Matrix.cpp
@@ -0,0 +1,15 @@
+class Solution {
+public:
+    void rotate(vector<vector<int>>& matrix) {
+        for(int i=0;i<matrix.size();i++){
+            for(int j=0;j<i;j++){
+                swap(matrix[i][j],matrix[j][i]);
+            }
+        }
+        for(int i=0;i<matrix.size();i++){
+            vector<int> t = matrix[i];
+            reverse(t.begin(),t.end());
+            matrix[i]=t;
+        }
+    }
+};
