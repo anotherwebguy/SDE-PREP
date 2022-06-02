6b69775774d66930422db2fa7192f9cf5101fe48
Mohit singh <mohitrajputms123@gmail.com>
2022-06-02 11:46:33 +0530

    Create Find the Duplicate Number.cpp

---
+++ Day 2: Arrays Part II/Find the Duplicate Number.cpp
@@ -0,0 +1,26 @@
+class Solution {
+public:
+    int findDuplicate(vector<int>& nums) {
+        int tortoise = nums[0];
+        int hare = nums[0];
+
+        do {
+            tortoise = nums[tortoise];
+            hare = nums[nums[hare]];
+        } while (tortoise != hare);
+
+        // Find the "entrance" to the cycle.
+        tortoise = nums[0];
+        while (tortoise != hare) {
+            tortoise = nums[tortoise];
+            hare = nums[hare];
+        }
+
+        return hare;
+    }
+};
+
+/*
+  O(N)-time
+  O(1)-space
+*/
