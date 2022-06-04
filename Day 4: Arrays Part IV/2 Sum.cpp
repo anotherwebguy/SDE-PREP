3f1d4e0e661a64de847d954aabbceb4b82d41fbd
Mohit singh <mohitrajputms123@gmail.com>
2022-06-04 10:14:34 +0530

    Create 2 Sum.cpp

---
+++ Day 4: Arrays Part IV/2 Sum.cpp
@@ -0,0 +1,17 @@
+class Solution {
+public:
+    vector<int> twoSum(vector<int>& nums, int target) {
+        map<int,int> m;
+        vector<int> res;
+        for(int i=0;i<nums.size();i++){
+            int x = target-nums[i];
+            if(m.find(x)!=m.end()){
+                res.push_back(m[x]);
+                res.push_back(i);
+                break;
+            }
+            m[nums[i]]=i;
+        }
+        return res;
+    }
+};
