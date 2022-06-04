e06ac8388efb6a1932b2d98a88e59b4171d7f8e1
Mohit singh <mohitrajputms123@gmail.com>
2022-06-04 11:15:30 +0530

    Create Largest Subarray with 0 sum.cpp

---
+++ Day 4: Arrays Part IV/Largest Subarray with 0 sum.cpp
@@ -0,0 +1,30 @@
+class Solution {
+public:
+    int longestConsecutive(vector<int>& nums) {
+        unordered_map<int,bool> m;
+        int n=nums.size();
+        for(int z=0;z<n;z++)
+        {
+            m[nums[z]]=1;
+        }
+        int ans=0;
+        for(int z=0;z<n;z++)
+        {
+            if(!m[nums[z]-1])
+            {
+                int k=nums[z];
+                int temp=0;
+                while(m[k])
+                {
+                    temp++;
+                    k++;
+                }
+                ans=max(ans,temp);
+            }
+        }
+        return ans;
+    }
+};
+
+// O(N)-time
+// O(N)-space
