f49c10e85fc3245ad8842ec8a7de4849cb241a2d
Mohit singh <mohitrajputms123@gmail.com>
2022-06-17 10:44:36 +0530

    Create Kth Largest Element.cpp

---
+++ Day12: Heaps/Kth Largest Element.cpp
@@ -0,0 +1,14 @@
+class Solution {
+public:
+    int findKthLargest(vector<int>& nums, int k) {
+        priority_queue<int> pq;
+        for(auto i:nums) pq.push(i);
+        k--;
+        while(k--){
+            pq.pop();
+        }
+        return pq.top();
+    }
+};
+
+//O(n)-for loop + O(n-k)log(k)
