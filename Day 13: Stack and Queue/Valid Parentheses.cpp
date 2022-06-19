bbde592c4c9bbc99c106ea0e963ad1d18e5a3285
Mohit singh <mohitrajputms123@gmail.com>
2022-06-19 10:36:03 +0530

    Create Valid Parentheses.cpp

---
+++ Day 13: Stack and Queue/Valid Parentheses.cpp
@@ -0,0 +1,20 @@
+class Solution {
+public:
+    bool isValid(string s) {
+        stack<char> st;
+        for(int i=0;i<s.size();i++){
+            if(s[i]=='(' || s[i]=='{' || s[i]=='['){
+                st.push(s[i]);
+            } else {
+                if(st.empty()) return false;
+                else if(s[i]==')' && st.top()!='(') return false;
+                else if(s[i]=='}' && st.top()!='{') return false;
+                else if(s[i]==']' && st.top()!='[') return false;
+                st.pop();
+            }
+        }
+        return st.empty();
+    }
+};
+
+//O(N)-time
