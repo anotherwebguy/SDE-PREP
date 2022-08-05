c940ed2871d55a209eaffa4f2bebfcfeed708010
Mohit singh <mohitrajputms123@gmail.com>
2022-08-05 11:30:11 +0530

    Create Target Sum.cpp

---
+++ Dynamic Programming Miscellaneous/Target Sum.cpp
@@ -0,0 +1,42 @@
+class Solution {
+public:
+    int findWays(vector<int> &num, int tar){
+     int n = num.size();
+
+        vector<vector<int>> dp(n,vector<int>(tar+1,0));
+
+        if(num[0] == 0) dp[0][0] =2;  // 2 cases -pick and not pick
+        else dp[0][0] = 1;  // 1 case - not pick
+
+        if(num[0]!=0 && num[0]<=tar) dp[0][num[0]] = 1;  // 1 case -pick
+
+        for(int ind = 1; ind<n; ind++){
+            for(int target= 0; target<=tar; target++){
+
+                int notTaken = dp[ind-1][target];
+
+                int taken = 0;
+                    if(num[ind]<=target)
+                        taken = dp[ind-1][target-num[ind]];
+
+                dp[ind][target]= (notTaken + taken);
+            }
+        }
+        return dp[n-1][tar];
+    }
+    
+    int findTargetSumWays(vector<int>& arr, int target) {
+        int totSum = 0;
+        int n = arr.size();
+        for(int i=0; i<n;i++){
+            totSum += arr[i];
+        }
+
+        //Checking for edge cases
+        if(totSum-target <0 || (totSum-target)%2 ) return 0;
+
+        return findWays(arr,(totSum-target)/2);
+    }
+
+};
+//same as partition with given difference
