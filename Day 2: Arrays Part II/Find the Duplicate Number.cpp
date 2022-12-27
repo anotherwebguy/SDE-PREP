5a1fdfd8fa2109c2428bd7bb1145b77604408388
Mohit singh <mohitrajputms123@gmail.com>
2022-12-27 11:58:14 +0530

    Update Find the Duplicate Number.cpp

--- Day 2: Arrays Part II/Find the Duplicate Number.cpp
+++ Day 2: Arrays Part II/Find the Duplicate Number.cpp
@@ -24,3 +24,25 @@ public:
   O(N)-time
   O(1)-space
 */
+
+
+/////////////////////////////////// java Solution /////////////////////////////////////////////////
+public class Solution {
+    // DO NOT MODIFY THE ARGUMENTS WITH "final" PREFIX. IT IS READ ONLY
+    public int[] repeatedNumber(final int[] A) {
+        int x = 0, y = 0,ans=0;
+        for(int i: A) ans^=i;
+        for(int i=1;i<=A.length;i++) ans^=i;
+        int setloc = ans & ~(ans-1);
+        for(int i:A){
+            if((i&setloc)!=0) x^=i;
+            else y^=i;
+        }
+        for(int i=1;i<=A.length;i++){
+            if((i&setloc)!=0) x^=i;
+            else y^=i;
+        }
+        for(int i:A) if(i==x) return new int[] {x,y};
+        return new int[] {y,x};
+    }
+}
