b9f6b13607647680bd816455cd453aef531816fc
Mohit singh <mohitrajputms123@gmail.com>
2024-07-16 10:35:58 +0530

    Update Find Missing and Repeating numbers.cpp

--- Day 2: Arrays Part II/Find Missing and Repeating numbers.cpp
+++ Day 2: Arrays Part II/Find Missing and Repeating numbers.cpp
@@ -19,3 +19,24 @@ vector<int> Solution::repeatedNumber(const vector<int> &A) {
   O(N)-space
   O(n)-time
 */
+
+vector<int> Solution::repeatedNumber(const vector<int> &A) {
+    long long n=A.size();
+    long long SUMn = (n*(n+1))/2;
+    long long SUM2n = (n*(n+1)*(2*n+1))/6;
+    long long SUM = 0, SUM2 = 0;
+    for(int i:A){
+        SUM += i;
+        SUM2 += (long long)i*(long long)i;
+    }
+    long long a = SUM2 - SUM2n;
+    long long b = SUM - SUMn;
+    long long repeat = (a/b + b)/2;
+    long long missing = (a/b - b)/2;
+    return {(int)repeat, (int)missing};
+}
+
+/*
+  O(1)-space
+  O(n)-time
+*/
