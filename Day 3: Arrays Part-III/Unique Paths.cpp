3d47532424aabdfdc7eb4e264a663394ef448784
Mohit singh <mohitrajputms123@gmail.com>
2024-07-18 10:05:24 +0530

    Update Unique Paths.cpp

--- Day 3: Arrays Part-III/Unique Paths.cpp
+++ Day 3: Arrays Part-III/Unique Paths.cpp
@@ -13,3 +13,20 @@ public:
 
 // O(N)-time
 // O(1)-space
+
+class Solution {
+public:
+    int uniquePaths(int m, int n) {
+        vector<vector<int>> dp(m,vector<int>(n,1));
+        for (int i=m-2;i>=0;i--){
+            for(int j = n-2;j>=0;j--){
+                dp[i][j] = dp[i+1][j]+dp[i][j+1];
+            }
+        }
+        return dp[0][0];
+    }
+
+};
+
+// O(M*N) - time
+//O(M*N) - space
