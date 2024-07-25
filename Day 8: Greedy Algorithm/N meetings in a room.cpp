571b6ad022266134c639802635ad5401219333b2
Mohit singh <mohitrajputms123@gmail.com>
2024-07-25 10:34:35 +0530

    Update N meetings in a room.cpp

--- Day 8: Greedy Algorithm/N meetings in a room.cpp
+++ Day 8: Greedy Algorithm/N meetings in a room.cpp
@@ -55,3 +55,58 @@ int main() {
 
 //O(N)+O(nlogn)+O(N)-time
 //O(N)-space
+
+//{ Driver Code Starts
+#include <bits/stdc++.h>
+using namespace std;
+
+// } Driver Code Ends
+class Solution
+{
+    public:
+    //Function to find the maximum number of meetings that can
+    //be performed in a meeting room.
+    bool static comparator(pair<int,int> a, pair<int,int> b){
+        if(a.second<b.second) return true;
+        else if(a.second>b.second) return false;
+        else return false;
+    }
+    
+    int maxMeetings(int start[], int end[], int n)
+    {
+        // Your code here
+        vector<pair<int,int>> ds;
+        for(int i=0;i<n;i++){
+            ds.push_back(make_pair(start[i],end[i]));
+        }
+        sort(ds.begin(),ds.end(),comparator);
+        int cnt = 1, last = ds[0].second;
+        for(int i=1;i<n;i++){
+            if(last<ds[i].first){
+                cnt++;
+                last = ds[i].second;
+            }
+        }
+        return cnt;
+    }
+};
+
+//{ Driver Code Starts.
+int main() {
+    int t;
+    cin >> t;
+    while (t--) {
+        int n;
+        cin >> n;
+        int start[n], end[n];
+        for (int i = 0; i < n; i++) cin >> start[i];
+
+        for (int i = 0; i < n; i++) cin >> end[i];
+
+        Solution ob;
+        int ans = ob.maxMeetings(start, end, n);
+        cout << ans << endl;
+    }
+    return 0;
+}
+// } Driver Code Ends
